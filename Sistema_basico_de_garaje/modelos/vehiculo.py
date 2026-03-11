aec243ec79a27ec54d6497da652bc66fe4de6579
JESSICA <ja.pujotap@uea.edu.ec>
2026-03-10 22:31:18 -0500

    Corrección de código inicial

--- Sistema_basico_de_garaje/modelos/vehiculo.py
+++ Sistema_basico_de_garaje/modelos/vehiculo.py
@@ -1,11 +1,11 @@
 class Vehiculo: #Representa un vehículo con su información básica.
 
-    def __init__(self, placa: str, marca: str, propietario: str):
+    def __init__(self, placa: str, marca: str, propietario: str): #Inicializa un nuevo vehículo con su placa, marca y propietario.
         self.placa = placa
         self.marca = marca
         self.propietario = propietario
 
-    def __str__(self):
+    def __str__(self): #Devuelve una representación en cadena del vehículo, mostrando su placa, marca y propietario.
         return f"{self.placa} - {self.marca} - {self.propietario}"
 
     def to_tuple(self): #Devuelve una tupla con los datos para insertar en la tabla de la UI
